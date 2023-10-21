5b8b1eac57d9a4844e52036cae3aafff89243903
David De Anda <omarandstuff@gmail.com>
2023-10-20 18:19:12 -0600

    runExpressApp util file

--- tests/express-controllers-fileupload.test.ts
+++ tests/express-controllers-fileupload.test.ts
@@ -1,24 +1,10 @@
-import { ExpressApp } from '@universal-packages/express-controllers'
-
 import { setFileUploadOptions } from '../src'
-
-const port = 4000 + Number(process.env['JEST_WORKER_ID'])
-
-let app: ExpressApp
-afterEach(async (): Promise<void> => {
-  await app.stop()
-})
 
 describe('express-controllers-fileupload', (): void => {
   it('It executed configured middleware all across controllers', async (): Promise<void> => {
-    app = new ExpressApp({ appLocation: './tests/__fixtures__', port })
-
     setFileUploadOptions({ tempFileDir: './tmp' })
 
-    await app.prepare()
-    await app.run()
-
-    app.on('request/error', console.log)
+    await runExpressApp()
 
     await fPost('good/1', fBuildFormData({}, { fileAttachment: './tests/__fixtures__/file.txt' }))
     expect(fResponse).toHaveReturnedWithStatus('OK')
