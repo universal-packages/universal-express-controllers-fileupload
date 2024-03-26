f6aff95f5c82c0510b1be403c73436c885388bd2
David De Anda <omarandstuff@gmail.com>
2024-03-25 18:17:54 -0600

    Use express controllers renameing

--- tests/express-controllers-fileupload.test.ts
+++ tests/express-controllers-fileupload.test.ts
@@ -4,7 +4,7 @@ describe('express-controllers-fileupload', (): void => {
   it('It executed configured middleware all across controllers', async (): Promise<void> => {
     setFileUploadOptions({ tempFileDir: './tmp' })
 
-    await runExpressApp()
+    await runExpressControllers()
 
     await fPost('good/1', fBuildFormData({}, { fileAttachment: './tests/__fixtures__/file.txt' }))
     expect(fResponse).toHaveReturnedWithStatus('OK')
